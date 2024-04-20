92f281841ad9da6a956ad824bc0688264b3ef9f8
asfasdfgasdfasdf@outlook.com <asfasdfgasdfasdf@outlook.com>
2024-04-20 14:22:29 +0500

    Добавьте файлы проекта.

---
+++ sklad/MainWindow.xaml.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using Newtonsoft.Json;
+using sklad;
+
+namespace Sklad
+{
+    public partial class MainWindow : Window
+    {
+        public class Product : INotifyPropertyChanged
+        {
+            private string name;
+            private DateTime date;
+            private int quantity;
+
+            public string Name
+            {
+                get { return name; }
+                set
+                {
+                    name = value;
+                    NotifyPropertyChanged();
+                }
+            }
+
+            public DateTime Date
+            {
+                get { return date; }
+                set
+                {
+                    date = value;
+                    NotifyPropertyChanged();
+                }
+            }
+
+            public int Quantity
+            {
+                get { return quantity; }
+                set
+                {
+                    quantity = value;
+                    NotifyPropertyChanged();
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public List<Product> Products { get; set; } = new List<Product>();
+        private const string dataFilePath = "data.json";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            LoadData();
+            auth auth = new auth();
+            auth.Close();
+        }
+
+        private void SaveData()
+        {
+            string jsonData = JsonConvert.SerializeObject(Products);
+            File.WriteAllText(dataFilePath, jsonData);
+        }
+        private void UpdateProductsListBox()
+        {
+            // Группировка продуктов по имени и суммирование количества
+            var groupedProducts = Products
+                .GroupBy(p => p.Name)
+                .Select(group => new Product
+                {
+                    Name = group.Key,
+                    Quantity = group.Sum(p => p.Quantity)
+                });
+
+            // Обновление списка продуктов в ListBox
+            productsListBox.ItemsSource = null;
+            productsListBox.ItemsSource = groupedProducts;
+        }
+
+        private void AddProductButton_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime currentDate = DateTime.Now;
+            int quantity = int.Parse(quantityTextBox.Text);
+
+            Product product = new Product
+            {
+                Name = productNameTextBox.Text,
+                Date = currentDate,
+                Quantity = quantity
+            };
+
+            Products.Add(product);
+            SaveData();
+            UpdateProductsListBox(); // Обновление ListBox после добавления продукта
+        }
+
+        private void RemoveProductButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (productsListBox.SelectedItem != null)
+            {
+                Product selectedProduct = (Product)productsListBox.SelectedItem;
+                string productName = selectedProduct.Name;
+                MessageBoxResult result = MessageBox.Show($"Удалить все данные о товаре {productName}?", "Удаление", MessageBoxButton.OKCancel);
+                if (result == MessageBoxResult.OK)
+                {
+                    int n = 0;
+
+                    // Создаем временную коллекцию для хранения элементов, которые нужно удалить
+                    List<Product> productsToRemove = new List<Product>();
+
+                    // Проходим по коллекции и добавляем в список элементы с нужным именем
+                    foreach (Product product in Products)
+                    {
+                        if (product.Name == productName)
+                        {
+                            productsToRemove.Add(product);
+                        }
+                    }
+
+                    // Удаляем все найденные элементы из основной коллекции
+                    foreach (Product productToRemove in productsToRemove)
+                    {
+                        Products.Remove(productToRemove);
+                        n++;
+                    }
+
+                    MessageBox.Show($"Удалено все движения товара {productName} в количестве {n} штук");
+                    SaveData();
+                    UpdateProductsListBox(); // Обновление ListBox после удаления продуктов
+                }         
+            }
+            else
+            {
+                MessageBox.Show("Выберите товар для удаления.", "Удаление");
+            }
+        }
+
+        private void LoadData()
+        {
+            if (File.Exists(dataFilePath))
+            {
+                string jsonData = File.ReadAllText(dataFilePath);
+                Products = JsonConvert.DeserializeObject<List<Product>>(jsonData);
+                UpdateProductsListBox(); // Обновление ListBox после загрузки данных
+            }
+            else
+            {
+                SaveData();
+            }
+        }
+
+        private void GenerateReportButton_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime startDate = DateTime.Now;
+            DateTime endDate = DateTime.Now;
+
+            switch (reportPeriodComboBox.SelectedIndex)
+            {
+                case 0: // Day
+                    startDate = DateTime.Today;
+                    break;
+                case 1: // Week
+                    startDate = DateTime.Today.AddDays(-7);
+                    break;
+                case 2: // Month
+                    startDate = DateTime.Today.AddDays(-30);
+                    break;
+            }
+
+            List<Product> filteredProducts = Products.Where(p => p.Date >= startDate && p.Date <= endDate).ToList();
+
+            string report = "Отчёт движения товаров:\n\n";
+            foreach (Product product in filteredProducts)
+            {
+                report += $"Товар: {product.Name}, Дата: {product.Date}, Кол-во: {product.Quantity}\n";
+            }
+
+            MessageBox.Show(report);
+        }
+
+        private void productsListBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            if (productsListBox.SelectedItem != null)
+            {
+                Product selectedProduct = (Product)productsListBox.SelectedItem;
+                productNameTextBox.Text = selectedProduct.Name;
+                productNameTextBox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void productNameTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (productNameTextBox.Text == "Товар")
+            {
+                productNameTextBox.Text = "";
+                productNameTextBox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void productNameTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (productNameTextBox.Text == "")
+            {
+                productNameTextBox.Text = "Товар";
+                productNameTextBox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void quantityTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (quantityTextBox.Text == "Кол-во")
+            {
+                quantityTextBox.Text = "";
+                quantityTextBox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void quantityTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (quantityTextBox.Text == "")
+            {
+                quantityTextBox.Text = "Кол-во";
+                quantityTextBox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+    }
+}
---
+++ sklad/auth.xaml.cs
@@ -0,0 +1,200 @@
+﻿using Newtonsoft.Json;
+using Sklad;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static Sklad.MainWindow;
+
+namespace sklad
+{
+    /// <summary>
+    /// Логика взаимодействия для auth.xaml
+    /// </summary>
+    public partial class auth : Window
+    {
+        public class User : INotifyPropertyChanged
+        {
+            private string login;
+            private string pass;
+
+            public string Login
+            {
+                get { return login; }
+                set
+                {
+                    login = value;
+                    NotifyPropertyChanged();
+                }
+            }
+
+            public string Pass
+            {
+                get { return pass; }
+                set
+                {
+                    pass = value;
+                    NotifyPropertyChanged();
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public List<User> Users { get; set; } = new List<User>();
+        private const string dataFilePath = "auth.json";
+        private const string readme = "readme";
+
+        public auth()
+        {
+            InitializeComponent();
+            DataContext = this;
+            LoadData();
+
+            if (File.Exists(readme)) // проверка на первый старт
+            {
+                string readmestr = File.ReadAllText(readme);
+                MessageBox.Show(readmestr, "рид ми");
+                string oldFilePath = "readme";
+                string newFileName = "readme.json";
+                File.Move(oldFilePath, newFileName);
+            }
+        }
+        private void SaveData()
+        {
+            string jsonData = JsonConvert.SerializeObject(Users);
+            File.WriteAllText(dataFilePath, jsonData);
+        }
+        private void LoadData()
+        {
+            if (File.Exists(dataFilePath))
+            {
+                string jsonData = File.ReadAllText(dataFilePath);
+                Users = JsonConvert.DeserializeObject<List<User>>(jsonData);
+            }
+            else
+            {
+                SaveData();
+            }
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string passHashed = CalculateSHA256(passwordTextbox.Text);
+            User user = Users.FirstOrDefault(u => u.Login == loginTextbox.Text && u.Pass == passHashed);
+
+            if (user != null)
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Неправильный логин/пароль", "Ошибка авторизации");
+                
+            }
+        }
+
+        private void registerButton_Click(object sender, RoutedEventArgs e)
+        {
+            string passHashed = CalculateSHA256(passwordTextbox.Text);
+            if (Users.Any(u => u.Login == loginTextbox.Text))
+            {
+                MessageBox.Show("Пользователь с таким логином уже существует", "Ошибка регистрации");
+                return;
+            }
+            User user = new User()
+            {
+                Login = loginTextbox.Text,
+                Pass = passHashed
+            };
+            MessageBox.Show("Пользователь добавлен");
+            Users.Add(user);
+            SaveData();
+        }
+        private string CalculateSHA256(string str)
+        {
+            SHA256 sha256 = SHA256Managed.Create();
+            byte[] hashValue;
+            UTF8Encoding objUtf8 = new UTF8Encoding();
+            hashValue = sha256.ComputeHash(objUtf8.GetBytes(str));
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (byte b in hashValue)
+            {
+                stringBuilder.Append(b.ToString("x2"));
+            }
+            return stringBuilder.ToString();
+        }
+
+        private void loginTextbox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (loginTextbox.Text == "Логин")
+            {
+                loginTextbox.Text = "";
+                loginTextbox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+        private void loginTextbox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (loginTextbox.Text == "")
+            {
+                loginTextbox.Text = "Логин";
+                loginTextbox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void passwordTextbox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (passwordTextbox.Text == "Пароль")
+            {
+                passwordTextbox.Text = "";
+                passwordTextbox.Foreground = System.Windows.Media.Brushes.Black;
+            }
+        }
+
+        private void passwordTextbox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (passwordTextbox.Text == "")
+            {
+                passwordTextbox.Text = "Пароль";
+                passwordTextbox.Foreground = System.Windows.Media.Brushes.Gray;
+            }
+        }
+
+        private void loginTextbox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                loginButton_Click(sender, e);
+            }
+        }
+
+        private void passwordTextbox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                loginButton_Click(sender, e);
+            }
+        }
+    }
+}
